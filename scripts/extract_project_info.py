7d3a35fa04fbca6849bbfc74922da140320c2bdd
JordanJWSmith <jordanjwsmith@gmail.com>
2023-10-21 14:20:49 +0100

    added notion pages and compiler script

---
+++ scripts/extract_project_info.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import os
+
+def extract_projects_info(filepath='/projects'):
+    project_info = []
+
+    for root, _, _ in os.walk(filepath):
+        if root != filepath:
+            # Extract subdirectory UUID
+            subdirectory_uuid = os.path.basename(root)
+
+            # Extract subdirectory contents
+            imagefolder = None
+            html_file = None
+            title_tag = None
+
+            for item in os.listdir(root):
+                item_path = os.path.join(root, item)
+
+                if os.path.isdir(item_path):
+                    imagefolder = item
+
+                if os.path.isfile(item_path) and item.endswith(".html"):
+                    html_file = item
+
+                    with open(item_path) as f:
+                        modal_content = f.read()
+                        soup = BeautifulSoup(modal_content, "html.parser")
+                        title_tag = soup.find("title").string
+            
+            if all([subdirectory_uuid, imagefolder, html_file, title_tag]):
+                # print('extracted', [subdirectory_uuid, imagefolder, html_file, title_tag])
+                # project_info['directory_uuid'] = subdirectory_uuid
+                # project_info['image_foldername'] = imagefolder
+                # project_info['html_filename'] = html_file
+                # project_info['title'] = title_tag
+                print('appending', title_tag)
+                project_info.append({
+                    'directory_uuid': subdirectory_uuid, 
+                    'image_foldername': imagefolder, 
+                    'html_filename': html_file, 
+                    'title': title_tag})
+
+    # print(project_info)
+    return project_info
+
+# Example usage
+# projects_info = extract_projects_info('./projects')
+
+# print(projects_info)
+# for info in projects_info:
+#     print(info)
+#     print()
--- scripts/generate_projects.py
+++ scripts/generate_projects.py
@@ -1,30 +1,62 @@
 from bs4 import BeautifulSoup
+from extract_project_info import extract_projects_info
+
+project_info = extract_projects_info('./projects')
 
 with open("./skeletons/projects/projects_head.html", "r") as f:
     html_content = f.read()
 
 soup = BeautifulSoup(html_content, "html.parser")
 
-for n in range(5):
+for project in project_info:
+
+    dir_uuid = project['directory_uuid']
+    image_foldername = project['image_foldername']
+    html_filename = project['html_filename']
+    title = project['title']
+
+    print('writing', title)
+
     project_card = f"""
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card">
             <img src="assets/images/ucl_compressed_2.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">{n}</h5>
-                <p class="card-text">{n}</p>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#{n}">More</button>
+                <h5 class="card-title">{title}</h5>
+                <p class="card-text">Info about {title}</p>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#{title}">More</button>
             </div>
         </div>
             </div>
     """
 
+    modal_content = f"""
+    <div class="modal fade" id="{title}" tabindex="-1" role="dialog" aria-labelledby="{title}Label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="{title}Label">{title}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                Content for {title} goes here.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+        """
+
+    
     container_to_edit = soup.find("div", {"id": "project_card_container"})
     container_to_edit.append(BeautifulSoup(project_card, "html.parser"))
 
-    new_div = soup.new_tag("div", id=f"modal={n}")
-    new_div.append(f"Sample text for new-div-{n} goes here.")
-    container_to_edit.insert_after(new_div)
+    new_modal = soup.new_tag("div", id=f"modal={title}")
+    new_modal.append(BeautifulSoup(modal_content, "html.parser"))
+    container_to_edit.insert_after(new_modal)
 
-    with open("test_output.html", "w") as output_file:
-        output_file.write(soup.prettify())
+with open("test_output.html", "w") as output_file:
+    output_file.write(soup.prettify())
+    print('output file written')
