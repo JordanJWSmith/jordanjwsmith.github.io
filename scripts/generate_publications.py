c5d33a801b5dfac98136f6fc34a9bf8f97adf0b9
JordanJWSmith <jordanjwsmith@gmail.com>
2023-10-23 22:13:26 +0100

    styling navbar active

--- scripts/generate_publications.py
+++ scripts/generate_publications.py
@@ -59,7 +59,7 @@ for publication in sorted_publication_descriptions:
     jumbotron = soup.find("div", {"id": "title_jumbotron"})
     jumbotron.insert_after(BeautifulSoup(publication_card, "html.parser"))
 
-with open("test_output.html", "w") as output_file:
+with open("publications.html", "w") as output_file:
     output_file.write(soup.prettify())
     print('output file written')
     
